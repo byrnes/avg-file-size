21171bdf4d5ec35b5acc8fb347535c6eb719a6dd
Connor Byrnes <connor@cbyrnes.io>
2021-03-09 10:35:05 -0500

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -68,18 +68,16 @@ fn avg_file_size(dir: PathBuf) -> (f64, f64) {
     let mut total_size: f64 = 0.0;
 
     if let Ok(files) = fs::read_dir(dir) {
-        files.for_each(|file| {
-            if let Ok(file) = file {
-                if let Ok(metadata) = file.metadata() {
-                    if metadata.is_file() {
-                        //println!("{:?} : {}", file.path(), metadata.len());
-                        file_count += 1.0;
-                        total_size += (metadata.len() / 1000) as f64;
-                    } else if metadata.is_dir() {
-                        let (sub_total_size, sub_file_count) = avg_file_size(file.path());
-                        file_count += sub_file_count;
-                        total_size += sub_total_size;
-                    }
+        files.for_each(|file| if let Ok(file) = file {
+            if let Ok(metadata) = file.metadata() {
+                if metadata.is_file() {
+                    //println!("{:?} : {}", file.path(), metadata.len());
+                    file_count += 1.0;
+                    total_size += (metadata.len() / 1000) as f64;
+                } else if metadata.is_dir() {
+                    let (sub_total_size, sub_file_count) = avg_file_size(file.path());
+                    file_count += sub_file_count;
+                    total_size += sub_total_size;
                 }
             }
         });
